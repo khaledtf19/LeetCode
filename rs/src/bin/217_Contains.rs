9a3282c07a7aea0a4374b9760f28c2cdae3564d4
Khaledtf Labeb <khaledtf19@gmail.com>
2024-01-01 15:13:38 +0200

    contains

---
+++ rs/src/bin/217_Contains.rs
@@ -0,0 +1,28 @@
+use std::collections::HashSet;
+
+fn main() {
+    let res = contains_duplicate(vec![1, 2, 3]);
+    println!("{}", res);
+}
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut hs: HashSet<i32> = HashSet::with_capacity(nums.len());
+    for num in nums.iter() {
+        let bol = hs.insert(*num);
+        match bol {
+            true => {}
+            false => return true,
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_contains() {
+        assert_eq!(contains_duplicate(vec![1, 2, 3, 1]), true)
+    }
+}
